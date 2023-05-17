9aefa3e9d5bf442b72a1cfe51995e16d4a8906f9
Matthias Hochsteger <mhochsteger@cerbsim.com>
2023-05-17 19:38:34 +0200

    yarn.lock

---
+++ rebuild.sh
@@ -0,0 +1,10 @@
+export DEV_BUILD=1
+set -e
+# pip uninstall -y webgui_jupyter_widgets
+rm -f dist/*.whl
+# rm -rf webgui/node_modules
+python setup.py bdist_wheel
+pip install dist/*.whl 
+jupyter nbextension install --user --py webgui_jupyter_widgets
+jupyter nbextension enable webgui_jupyter_widgets --user --py
+
